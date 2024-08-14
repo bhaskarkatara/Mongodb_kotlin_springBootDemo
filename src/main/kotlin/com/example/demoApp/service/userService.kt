9cd6b1404cf6a3d17830e5108406442fa51b9922
bhaskar katara <bhaskarkatararjo5@gmail.com>
2024-08-14 12:55:48 +0530

    create: user Authentication

---
+++ src/main/kotlin/com/example/demoApp/service/userService.kt
@@ -0,0 +1,29 @@
+package com.example.demoApp.service
+
+import com.example.demoApp.entity.DataModel
+import com.example.demoApp.entity.UserDataModel
+import com.example.demoApp.repository.DemoEntryRepo
+import com.example.demoApp.repository.userRepo
+import org.apache.catalina.User
+import org.bson.types.ObjectId
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+import java.util.*
+
+@Component
+class userService {
+
+    // dependency injection
+    @Autowired
+    lateinit var  userRepo: userRepo
+
+    fun saveEntries(entry: UserDataModel){
+        userRepo.save(entry)
+    }
+
+    fun getAll(): List<UserDataModel> = userRepo.findAll()
+
+    fun getById(id: ObjectId): Optional<UserDataModel> = userRepo.findById(id)
+
+    fun deleteById(id: ObjectId) = userRepo.deleteById(id)
+}
